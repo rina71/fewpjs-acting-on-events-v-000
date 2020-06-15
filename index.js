fbee3c7db2ccf014eb3ec7738168a38c18ff0798
Yurina Shin <yurinas@gmail.com>
2020-06-15 01:14:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code here
+function moveDodgerLeft(){
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
